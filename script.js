bea8ac51e8be08a35253808f28205624b9524dfd
tomashm01 <tomashidalgomartin@gmail.com>
2021-12-17 00:32:23 +0100

    interfaz con patron diseño Object Literal

--- script.js
+++ script.js
@@ -3,66 +3,107 @@
  * @author Tomás
  */
 {
-document.addEventListener("DOMContentLoaded", function(){
+    let calculadora={
+        signos:["CE","<-","%","+",7,8,9,"-",4,5,6,"x",1,2,3,"÷",0,"±",",","="],
+        input:null,
+        divPrincipal:null,
+        resultado:null,
+        init(){
+            calculadora.crearInput();
+            calculadora.crearDiv();
+            calculadora.estructuraBotones();
+        },
+        crearInput(){
+            calculadora.input = document.createElement("input");
+            calculadora.input.setAttribute("type", "text");
+            calculadora.input.setAttribute("value", "0");
+            calculadora.input.style.width="230px";
+            calculadora.input.style.textAlign="right";
+            calculadora.input.style.margin="5px";
+            calculadora.input.disabled = true;
+        },
+        crearDiv(){
+            calculadora.divPrincipal= document.createElement("div");
+            calculadora.divPrincipal.appendChild(calculadora.input);
+            calculadora.divPrincipal.appendChild(document.createElement("br"));
+        },
+        estructuraBotones(){
+            let contador=0;
 
-    //Boton para introducir un número
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("value", "0");
-    input.style.width="230px";
-    input.style.textAlign="right";
-    input.style.margin="5px";
-    input.disabled = true;
+            for(valor of calculadora.signos){
+                calculadora.divPrincipal.appendChild(calculadora.crearBoton(valor));
+                contador++;
+                if(contador===4){
+                    calculadora.divPrincipal.appendChild(document.createElement("br"));
+                    contador=0;
+                }
+            }
+        },
+        crearBoton(valor){
 
-    //Div donde está todo
-    let divPrincipal= document.createElement("div");
-    divPrincipal.appendChild(input);
-    divPrincipal.appendChild(document.createElement("br"));
-
-    //Array con los signos
-    let signos=["CE","<-","%","+",7,8,9,"-",4,5,6,"x",1,2,3,"÷",0,"±",",","="];
-    let contador=0;
-
-    for(signo of signos){
-        divPrincipal.appendChild(crearBoton(signo));
-        contador++;
-        if(contador===4){
-            divPrincipal.appendChild(document.createElement("br"));
-            contador=0;
+            let boton = document.createElement("button");
+            boton.setAttribute("type", "button");
+            boton.setAttribute("id", valor);
+    
+            boton.style.width="50px";
+            boton.style.margin="5px";
+            boton.innerHTML=valor;
+    
+            return boton;
+    
         }
     }
 
-    //Insertar interfaz
-    document.open();
-    document.appendChild(divPrincipal);
+    document.addEventListener("DOMContentLoaded", function(){
+
+        calculadora.init();
+        document.body.appendChild(calculadora.divPrincipal);
+        document.close();
+
+        
+    });
+
+    //Realiza la operación correspondiente
+    // const operacion=(valor,input)=>{
+    //     switch(valor){
+    //         case "CE": //Borrar todo
+
+    //             input.value="0";
+    //             break;
+    //         case "&lt;-": //Borrar un caracter
+
+    //             if(input.value.length===0){
+    //                 input.value="0";
+    //             }else{
+    //                 input.value=valorInput.substring(0,valorInput.length-1);
+    //             }              
+
+    //             break;
+    //         case "%": //Hacer modulo
+    //             if(input.value.length===0){
+    //                 break;
+    //             }else{
+    //                 input.value=input.value%100;
+    //             }   
+    //             break;
+    //         case "+":
+    //             input.value=parseFloat(valorInput)+parseFloat(input.value);
+    //             break;
+    //         case "-":
+    //             input.value=parseFloat(valorInput)-parseFloat(input.value);
+    //             break;
+    //         case "x":
+    //             input.value=parseFloat(valorInput)*parseFloat(input.value);
+    //             break;
+    //         case "÷":
+    //             input.value=parseFloat(valorInput)/parseFloat(input.value);
+    //             break;
+    //         case "=":
+    //             input.value=eval(input.value);
+    //             break;
+    //     }
+    // }
+
+    //Funcion para crear un boton
     
-
-    //Agregar funcionalidad
-    let botones=document.getElementsByTagName("button");
-
-    //Recorro cada boton y veo si ha sido pulsado para ejecutar la funcion correspondiente
-    // botones.forEach(boton=>{
-    //     boton.addEventListener("click", function(){
-    //         input.value=this.value;
-    //     });
-    // });
-
-
-    document.close();
-});
-
-//Funcion para crear un boton
-const crearBoton=(valor)=>{
-
-    let boton = document.createElement("button");
-    boton.setAttribute("type", "button");
-    boton.setAttribute("id", valor);
-
-    boton.style.width="50px";
-    boton.style.margin="5px";
-    boton.innerHTML=valor;
-
-    return boton;
-
-}
 }
