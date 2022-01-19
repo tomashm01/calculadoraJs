6479abdda6811de139a101fd7ab1517ebae7dc26
tomashm01 <tomashidalgomartin@gmail.com>
2022-01-20 00:10:24 +0100

    close #4 , #5 fixed: reset 0 y return comportamiento

--- script.js
+++ script.js
@@ -23,97 +23,92 @@
             calculadora.crearInput();
             calculadora.crearDiv();
             calculadora.estructuraBotones();
-            document.body.appendChild(calculadora.divPrincipal);
-            let botones=document.getElementsByTagName("button");
-            for(let i=0;i<botones.length;i++){
-                botones[i].addEventListener('click',calculadora.comportamiento);
-            }
-            
+            document.body.appendChild(calculadora.divPrincipal);            
         },
-        comportamiento(e){
-            let valor=e.target.innerHTML;
+        comportamiento(valor){
             if(calculadora.input.value=="Error" && valor!="CE"){
-                return;
+                return null;
             }
-            switch(valor){
-                case "0":
-                    if(calculadora.nuevoValor){
-                        calculadora.input.value=valor;
-                        calculadora.nuevoValor=false;
-                    }else if(calculadora.input.value!=0||calculadora.input.value.length==0||calculadora.input.value.includes(".")){
-                        calculadora.input.value+=valor;
-                    }
-                    break;
-                case "1": 
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
+            if(valor<="9" && valor>="1"){
+                return()=>{
                     if((calculadora.input.value==0 && calculadora.input.value.length<=1)||calculadora.nuevoValor){
                         calculadora.input.value=valor;
                     }else{
                         calculadora.input.value+=valor;
                         calculadora.nuevoValor=false;
                     }
-                    break;
+                }
+            }
+            switch(valor){
+                case "0":
+                    return ()=>{
+                        if(calculadora.nuevoValor){
+                            calculadora.input.value=valor;
+                            calculadora.nuevoValor=false;
+                        }else if(calculadora.input.value!=0||calculadora.input.value.length==0||calculadora.input.value.includes(".")){
+                            calculadora.input.value+=valor;
+                        }
+                    }
                 case "±":
-                    if(calculadora.input.value>0){
-                        calculadora.input.value = -Math.abs(calculadora.input.value);
+                    return ()=>{
+                        if(calculadora.input.value>0){
+                            calculadora.input.value = -Math.abs(calculadora.input.value);
+                        }
+                        else{
+                            calculadora.input.value=Math.abs(calculadora.input.value);
+                        }
                     }
-                    else{
-                        calculadora.input.value=Math.abs(calculadora.input.value);
-                    }
-                    break;
                 case "&lt;-": //<-
-                    calculadora.input.value=calculadora.input.value.substring(0,calculadora.input.value.length-1);
-                    if(calculadora.input.value==""){
-                        calculadora.input.value="0";
+                    return()=>{
+                        calculadora.input.value=calculadora.input.value.substring(0,calculadora.input.value.length-1);
+                        if(calculadora.input.value==""){
+                            calculadora.input.value="0";
+                        }
                     }
-                    break;
                 case ",":
-                    if(!calculadora.input.value.includes(".")){
-                        calculadora.input.value+=".";
+                    return()=>{
+                        if(!calculadora.input.value.includes(".")){
+                            calculadora.input.value+=".";
+                        }
                     }
-                    break;
                 case "CE":
-                    calculadora.input.value="0";
-                    calculadora.valor1=0;
-                    calculadora.valor2=0;
-                    calculadora.resetearOperadores();
-                    calculadora.operacionActual=false;
-                    calculadora.operacionPosterior=false;
-                    calculadora.primerNumero=true;
-                    break;
+                    return()=>{
+                        calculadora.input.value="0";
+                        calculadora.valor1=0;
+                        calculadora.valor2=0;
+                        calculadora.resetearOperadores();
+                        calculadora.operacionActual=false;
+                        calculadora.operacionPosterior=false;
+                        calculadora.primerNumero=true;
+                    }
                 case "=":
-                    if (calculadora.operacionActual == false && calculadora.operacionPosterior != "+" || calculadora.operacionPosterior != "-" || calculadora.operacionPosterior != "x" || calculadora.operacionPosterior != "/")
-                        calculadora.asignarOperaciones(calculadora.operacionPosterior);
-                    calculadora.comprobarOperacion(calculadora.input.value);
-                    calculadora.nuevoValor = true;
-                    break;
+                    return()=>{
+                        if (calculadora.operacionActual == false && calculadora.operacionPosterior != "+" || calculadora.operacionPosterior != "-" || calculadora.operacionPosterior != "x" || calculadora.operacionPosterior != "/")
+                            calculadora.asignarOperaciones(calculadora.operacionPosterior);
+                        calculadora.comprobarOperacion(calculadora.input.value);
+                        calculadora.nuevoValor = true;
+                    }
                 case "+":
                 case "-":
                 case "x":
                 case "÷":
                 case "%":
-                    if (calculadora.primerNumero) {
-                        calculadora.valor1 = calculadora.input.value;
-                        calculadora.operacionPosterior = valor;
-                        calculadora.primerNumero = false;
-                    } else {
-                        calculadora.valor2 = calculadora.input.value;
-                        calculadora.realizarOperacion = true;
-                        calculadora.operacionActual = calculadora.operacionPosterior;
-                        calculadora.operacionPosterior = valor;
+                    return()=>{
+                        if (calculadora.primerNumero) {
+                            calculadora.valor1 = calculadora.input.value;
+                            calculadora.operacionPosterior = valor;
+                            calculadora.primerNumero = false;
+                        } else {
+                            calculadora.valor2 = calculadora.input.value;
+                            calculadora.realizarOperacion = true;
+                            calculadora.operacionActual = calculadora.operacionPosterior;
+                            calculadora.operacionPosterior = valor;
+                        }
+                        calculadora.asignarOperaciones(calculadora.operacionActual);
+                        if (calculadora.realizarOperacion && !calculadora.nuevoValor)
+                            calculadora.comprobarOperacion(calculadora.input.value);
+                        calculadora.nuevoValor = true;
                     }
-                    calculadora.asignarOperaciones(calculadora.operacionActual);
-                    if (calculadora.realizarOperacion && !calculadora.nuevoValor)
-                        calculadora.comprobarOperacion(calculadora.input.value);
-                    calculadora.nuevoValor = true;
-                    break;
             }
         },
         comprobarOperacion(valor){
@@ -186,7 +181,6 @@
         },
         estructuraBotones(){
             let contador=0;
-
             for(valor of calculadora.signos){
                 calculadora.divPrincipal.appendChild(calculadora.crearBoton(valor));
                 contador++;
@@ -197,13 +191,12 @@
             }
         },
         crearBoton(valor){
-
             let boton = document.createElement("button");
             boton.setAttribute("type", "button");   
             boton.style.width="50px";
             boton.style.margin="5px";
             boton.textContent=valor;
-    
+            boton.addEventListener("click",calculadora.comportamiento(valor));
             return boton;
     
         }
